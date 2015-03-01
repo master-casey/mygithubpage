ac54160e9ed54ac5d9460a3d94e38b92bbe4d4c8
Casey <cyh4mw@virginia.edu>
2015-02-28 21:52:37 -0500

    i forget what jquery is

--- javascripts/script.js
+++ javascripts/script.js
@@ -2,16 +2,16 @@
 $(document).ready(function(){
 
 
+
+  })
+})(jQuery)
+
   var excuses = [
 	"Are you bulking? Can you teach me your tricks?",
 	"I'm sorry, do you have the time? Oh shit - I'm late - ",
 	"k.",
 	"I can't relate."
   ];
-
-  })
-})(jQuery)
-
 
 function getExcuse() {
 	var excuse = excuses[Math.floor(Math.random() * excuses.length)];
