23adc5c2720071bf1c39c4108ec069b182259c49
master-casey <casey@localhost.(none)>
2015-03-19 00:22:05 -0400

    write

--- javascripts/sujin.js
+++ javascripts/sujin.js
@@ -13,11 +13,16 @@ $(document).ready(function(){
   xml.send();
   var xmlDoc = xml.responseXML;
 
-  var counter = parseInt(xmlDoc.getElementsByTagName("count")[0].childNodes[0].nodeValue);
+  var node = xmlDoc.getElementsByTagName("count")[0].childNodes[0];
+  var counter = parseInt(node.nodeValue);
+
+  var writer = new XMLWriter();
+
 
 function incr() {
 	counter = counter + 1;
 	var div = document.getElementById("name_b");
 	div.textContent = counter;
 	var text = div.textContent;
+	node.nodeValue = counter;
   }
